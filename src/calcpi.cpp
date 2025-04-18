27f9a73ec768c8b6dd87708bbf852b425211e420
Clas Onnebrink <clas@gmx.net>
2025-04-18 21:47:11 +0200

    first prepare

--- src/calcpi.cpp
+++ src/calcpi.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <algorithm>
 
 const int R_SUCCESS = 0;
 const int R_INVALID_CONDITIONS = -1;
@@ -20,22 +21,48 @@ enum CalcPiType {
 struct CalcRunConditions {
   CalcPiType type;
   long iterations;
+  bool showGraph;
   bool isComplete;
 };
 
-CalcRunConditions readRunConditions(int argCount, char argStr) {
+CalcRunConditions readRunConditions(int argCount, char **argStr) {
   CalcRunConditions runConditions;
   runConditions.isComplete = false;
+  runConditions.showGraph = false;
+  for (int i = 0; i < argCount; ++i) {
+    string param {argStr[i]};
+    transform(param.begin(), param.end(), param.begin(),
+	      [](auto c){return tolower(c);});
+    if (param.find("-t:") != string::npos) {
+      string cType {param.substr(3,param.length())};
+      if (cType == "viete")
+	runConditions.type = CalcPiType::CON_VIETE;
+      if (cType == "leibniz")
+	runConditions.type = CalcPiType::CON_LEIBNIZ;
+      if (cType == "euler")
+	runConditions.type = CalcPiType::CON_EULER;
+      if (cType == "rama")
+	runConditions.type = CalcPiType::CON_RAMA;
+      if (cType == "normal")
+	runConditions.type = CalcPiType::GRP_NORMAL;
+      if (cType == "montecarlo")
+	runConditions.type = CalcPiType::GRP_MONTECARLO;
+    }
+    if (param.find("-n:") != string::npos)
+      runConditions.iterations = stol(param.substr(3,param.length()));
+    if (param.find("-g") != string::npos)
+      runConditions.showGraph = true;
+  }
   return runConditions;
 }
 
 void printHelp() {
-  cout << "calcpi -t:<type> -n:<iterations>\n";
+  cout << "calcpi -t:<type> -n:<iterations> -g\n";
 }
 
 int main(int argc, char *argv[])
 {
-  auto runConditions = readRunConditions(argc, argc);
+  auto runConditions = readRunConditions(argc, argv);
   if (runConditions.isComplete == false) {
     printHelp();
     return R_INVALID_CONDITIONS;
